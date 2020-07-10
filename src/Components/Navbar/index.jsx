27e048c4ac05886cc692d72384dc6c201f15f288
Luckzman <lumpsey@gmail.com>
2020-07-11 00:16:18 +0100

    implement design for navbar

---
+++ src/Components/Navbar/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { ReactComponent as Logo } from '../../Assets/logo.svg';
+import { ReactComponent as SearchIcon } from '../../Assets/Icon-map-search.svg';
+import './Navbar.scss';
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="container">
+        <div className="brand">
+          <Logo className="logo" />
+        </div>
+        <div className="nav">
+          <ul className="left-nav">
+            <li>About Us</li>
+            <li>How it Works</li>
+            <li>MQN</li>
+            <li>Team</li>
+            <li>Contact</li>
+            <li>Blog</li>
+            <li>Careers</li>
+            <li>Resource</li>
+          </ul>
+          <ul className="right-nav">
+            <li>Register</li>
+            <li>Login</li>
+            <li>
+              <input type="text" placeholder="Search" />
+              <SearchIcon className="search-icon" />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
