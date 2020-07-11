a009cbea813c4205860de8fca9b773d248ac9d8a
Luckzman <lumpsey@gmail.com>
2020-07-11 02:38:42 +0100

    implement banner component on homepage

---
+++ src/Components/Banner/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './Banner.scss';
+
+const Banner = () => {
+  return (
+    <div className="Banner">
+      <div className="container">
+        <h1 className="highlight-text">Your Health in Your Hands.</h1>
+        <h1 className="highlight-text secondary">Anytime. Anywhere.</h1>
+        <button>Get Started</button>
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
